25dac3cf3e66d2b3dea986983e825a3835fb460e
Rolf Fux <fendant@outlook.com>
2016-02-24 23:21:23 +0100

    Created DB controller

--- src/SharpTsdb/DbController.cs
+++ src/SharpTsdb/DbController.cs
@@ -1,18 +1,48 @@
-﻿using System.Web.Http;
-using DbInterfaces.Interfaces;
-using FileDb.InterfaceImpl;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
 using SharpTsdb.Properties;
+using Timeenator.Impl;
 
 namespace SharpTsdb
 {
     [RoutePrefix("")]
     public class DbController : ApiController
     {
-        [Route("createDb")]
+        [Route("dbs/createDb/{dbName}")]
         [HttpGet]
         public string CreateDb(string dbName)
         {
             DbService.DbManagement.CreateDb(Settings.Default.DbDirectory, dbName);
+            return "ok";
+        }
+
+        [Route("db/{dbName}/createMeasurment/{name}")]
+        [HttpGet]
+        public string CreateMeasurement(string dbName, string name)
+        {
+            var myDb = DbService.DbManagement.GetDb(dbName);
+            myDb.CreateMeasurement(name, typeof(float));
+            return "ok";
+        }
+
+        [Route("db/{dbName}/clearMeasurment/{name}")]
+        [HttpGet]
+        public string ClearMeasurement(string dbName, string name, DateTime? after)
+        {
+            var myDb = DbService.DbManagement.GetDb(dbName);
+            myDb.GetMeasurement(name).ClearDataPoints(after);
+            return "ok";
+        }
+
+        [Route("db/{dbName}/{meas}/write")]
+        [HttpPost]
+        public string WritePoints(string dbName, string meas, [FromBody]List<WritePoint> points)
+        {
+            var myDb = DbService.DbManagement.GetDb(dbName);
+            var measurement = myDb.GetOrCreateMeasurement(meas);
+            measurement.AppendDataPoints(points.Select(i => new DataRow() { Key = i.t, Value = i.v }));
             return "ok";
         }
     }
--- src/SharpTsdb/GrafanaController.cs
+++ src/SharpTsdb/GrafanaController.cs
@@ -20,36 +20,12 @@ namespace SharpTsdb
         {
             return _handler.HandleQuery(db, q, DbService.DbManagement);
         }
+       
 
+
+
+       
 
         
-
-        [Route("createMeas")]
-        [HttpGet]
-        public string CreateMeas(string db, string name)
-        {
-            var myDb = DbService.DbManagement.GetDb(db);
-            myDb.CreateMeasurement(name, typeof (float));
-            return "ok";
-        }
-
-        [Route("clearMeas")]
-        [HttpGet]
-        public string ClearMeas(string db, string name, DateTime? after)
-        {
-            var myDb = DbService.DbManagement.GetDb(db);
-            myDb.GetMeasurement(name).ClearDataPoints(after);
-            return "ok";
-        }
-
-        [Route("write")]
-        [HttpPost]
-        public string WritePoints(string db, string meas, [FromBody]List<WritePoint> points)
-        {
-            var myDb = DbService.DbManagement.GetDb(db);
-            var measurement = myDb.GetOrCreateMeasurement(meas);
-            measurement.AppendDataPoints(points.Select(i => new DataRow() {Key = i.t, Value = i.v}));
-            return "ok";
-        }
     }
 }
