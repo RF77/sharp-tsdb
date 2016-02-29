5d50dff93c3d3affb7cb3aeca7e4a05ba7489640
Rolf Fux <fendant@outlook.com>
2016-02-29 08:37:43 +0100

    nuget

--- src/Timeenator/Properties/AssemblyInfo.cs
+++ src/Timeenator/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Timeenator")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library for time series")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("RF77")]
 [assembly: AssemblyProduct("Timeenator")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © RF77 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.*")]
+[assembly: AssemblyFileVersion("0.0.1")]
